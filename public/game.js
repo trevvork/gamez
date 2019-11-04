f4d68caff5de38cc580c58878136c08309a23175
Michael Ohtake <mikado98765@gmail.com>
2019-11-04 12:48:54 -0500

    Added bounds to game, updated gitignore

--- public/game.js
+++ public/game.js
@@ -1,11 +1,20 @@
 var canvas = document.getElementById('myCanvas');
 var label = document.getElementById('score');
 var ctx = canvas.getContext('2d');
+var xDim = 500;
+var yDim = 300;
 var raf;
+var deltaX = 0;
+var deltaY = 0;
+var score = 0;
+var score = 0;
+var randomX = Math.floor(Math.random() * Math.floor(canvas.width - 30)) + 30;
+var randomY = Math.floor(Math.random() * Math.floor(canvas.height - 30)) + 30;
+label.innerText = score;
 
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(500 + deltaX, 300 + deltaY, 25, 0, Math.PI * 2, true);
+    ctx.arc(xDim + deltaX, yDim + deltaY, 25, 0, Math.PI * 2, true);
     ctx.closePath();
     ctx.fillStyle = "blue";
     ctx.fill();
@@ -24,39 +33,34 @@ function drawX(x , y) {
     ctx.stroke();
 }
 
-var deltaX = 0;
-var deltaY = 0;
-
-var randomX = Math.floor(Math.random() * Math.floor(canvas.width - 30)) + 30;
-var randomY = Math.floor(Math.random() * Math.floor(canvas.height - 30)) + 30;
-
-var score = 0;
-label.innerText = score;
-
-
-
-window.addEventListener("keydown", moveSomething, false);
-  
 function moveSomething(e) {
     switch(e.keyCode) {
         case 37:
-            deltaX -= 10;
+            if(xDim - deltaX < xDim * 2){
+                deltaX -= 10;
+            }
             break;
         case 38:
-            deltaY -= 10;
+            if(yDim - deltaY < yDim * 2){
+                deltaY -= 10;
+            }
             break;
         case 39:
-            deltaX += 10;
+            if(xDim - deltaX > 0){
+                deltaX += 10;
+            }
             break;
         case 40:
-            deltaY += 10;
+            if(yDim - deltaY > 0){
+                deltaY += 10;
+            }
             break;  
     }   
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawX(randomX,randomY);
     drawBall();
-    if ( (500 + deltaX - randomX < 25 && 500 + deltaX - randomX > -25) &&
-         (300 + deltaY - randomY < 25 && 300 + deltaY - randomY > -25) ){
+    if ( (xDim + deltaX - randomX < 25 && xDim + deltaX - randomX > -25) &&
+         (yDim + deltaY - randomY < 25 && yDim + deltaY - randomY > -25) ){
         score += 1;
         randomX = Math.floor(Math.random() * Math.floor(canvas.width - 30)) + 30;
         randomY = Math.floor(Math.random() * Math.floor(canvas.height - 30)) + 30;
@@ -67,5 +71,6 @@ function moveSomething(e) {
     label.innerText = score;
 }
 
+window.addEventListener("keydown", moveSomething, false);
 drawBall();
 
