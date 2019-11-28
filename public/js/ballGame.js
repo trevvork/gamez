9bbdf65f6fa94f7933234e848e3a4bd9c4e24b09
Trevor Kornaga <trevor4nowk@gmail.com>
2019-11-28 00:45:32 -0500

    Added background color, ball speed up boost, end game text

--- public/js/ballGame.js
+++ public/js/ballGame.js
@@ -6,22 +6,89 @@ var ctx = canvas.getContext('2d');
 var xDim = 500;
 var yDim = 300;
 var raf;
-var deltaX = 0;
-var deltaY = 0;
+var speedX = 0;
+var speedY = 0;
 var score = 0;
 var timePassed = 0;
 var GAME_LENGTH = 30; //seconds
+var SPEED = 1;
+var MAX_SPEED = 10;
 var progressPercent = 0;
-var randomX = Math.floor(Math.random() * Math.floor(canvas.width - 30)) + 30;
-var randomY = Math.floor(Math.random() * Math.floor(canvas.height - 30)) + 30;
+var randomEnemyX = Math.floor(Math.random() * Math.floor(canvas.width - 30)) + 30;
+var randomEnemyY = Math.floor(Math.random() * Math.floor(canvas.height - 30)) + 30;
+
+var randomSpeedX = Math.floor(Math.random() * Math.floor(canvas.width - 30)) + 30;
+var randomSpeedY = Math.floor(Math.random() * Math.floor(canvas.height - 30)) + 30;
 label.innerText = score;
 var pointSound;
+var gameOver = false;
+
+ctx.fillStyle = '#3C3C3C';
+ctx.fillRect(0,0,canvas.width, canvas.height);
+
+var interval;
 
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(xDim + deltaX, yDim + deltaY, 25, 0, Math.PI * 2, true);
+    xDim += speedX;
+    yDim += speedY;
+    if(yDim > canvas.height){
+        yDim = canvas.height;
+    }
+    else if (yDim < 0){
+        yDim = 0;
+    }
+    if(xDim > canvas.width){
+        xDim = canvas.width;
+    }
+    else if (xDim < 0){
+        xDim = 0;
+    }
+    ctx.arc(xDim, yDim, 25, 0, Math.PI * 2, true);
     ctx.closePath();
     ctx.fillStyle = "blue";
+    ctx.fill();
+};
+
+function update (){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    drawBall();
+    drawX(randomEnemyX,randomEnemyY);
+    drawSpeed(randomSpeedX, randomSpeedY);
+    if ( (xDim - randomEnemyX < 25 && xDim - randomEnemyX > -25) &&
+         (yDim - randomEnemyY < 25 && yDim - randomEnemyY > -25) ){
+        score += 1;
+        randomEnemyX = Math.floor(Math.random() * Math.floor(canvas.width - 30)) + 30;
+        randomEnemyY = Math.floor(Math.random() * Math.floor(canvas.height - 30)) + 30;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        drawBall();
+        drawX(randomEnemyX,randomEnemyY);
+    }
+    if ( (xDim - randomSpeedX < 25 && xDim - randomSpeedX > -25) &&
+         (yDim - randomSpeedY < 25 && yDim - randomSpeedY > -25) ){
+        SPEED = SPEED + 1 > MAX_SPEED ? MAX_SPEED : SPEED + 1;
+        randomSpeedX = Math.floor(Math.random() * Math.floor(canvas.width - 30)) + 30;
+        randomSpeedY = Math.floor(Math.random() * Math.floor(canvas.height - 30)) + 30;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        drawBall();
+        drawX(randomEnemyX,randomEnemyY);
+        drawSpeed(randomSpeedX, randomSpeedY);
+    }
+    label.innerText = score;
+    if (gameOver){
+        ctx.fillStyle = "red";
+        ctx.font = "30px Arial";
+        ctx.fillText("GAME OVER\nScore:" + score, canvas.width/2, canvas.height/2);
+        clearInterval(interval);
+    }
+}
+
+function drawSpeed(x, y) {
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI * 2, true);
+    ctx.closePath();
+    ctx.fillStyle = "yellow";
     ctx.fill();
 };
 
@@ -38,50 +105,48 @@ function drawX(x , y) {
     ctx.stroke();
 }
 
+
+
 function moveSomething(e) {
-    
     switch(e.keyCode) {
         case 37:
-            if(xDim - deltaX < xDim * 2){
-                deltaX -= 10;
-            }
+            speedX = -SPEED;
             break;
         case 38:
-            if(yDim - deltaY < yDim * 2){
-                deltaY -= 10;
-            }
+            speedY = -SPEED;
             break;
         case 39:
-            if(xDim - deltaX > 0){
-                deltaX += 10;
-            }
+            speedX = SPEED;
             break;
         case 40:
-            if(yDim - deltaY > 0){
-                deltaY += 10;
-            }
+            speedY = SPEED;
             break;  
     }   
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawX(randomX,randomY);
-    drawBall();
-    if ( (xDim + deltaX - randomX < 25 && xDim + deltaX - randomX > -25) &&
-         (yDim + deltaY - randomY < 25 && yDim + deltaY - randomY > -25) ){
-        score += 1;
-        randomX = Math.floor(Math.random() * Math.floor(canvas.width - 30)) + 30;
-        randomY = Math.floor(Math.random() * Math.floor(canvas.height - 30)) + 30;
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        drawBall();
-        drawX(randomX,randomY);
-    }
-    label.innerText = score;
 }
+function stopSomething(e) {
+    switch(e.keyCode) {
+        case 37:
+            speedX = 0;
+            break;
+        case 38:
+            speedY = 0;
+            break;
+        case 39:
+            speedX = 0;
+            break;
+        case 40:
+            speedY = 0;
+            break;  
+    }   
+}
+
 
 function startGame(){
     drawBall();
     progressBar.innerHTML = GAME_LENGTH;
     progressBar.style.width = "100%";
     timePassed += 1;
+    interval = setInterval(update, 20);
 }
 
 var gameActive = setInterval(function() {
@@ -91,7 +156,7 @@ var gameActive = setInterval(function() {
         progressBar.innerHTML = 0;
         clearInterval(gameActive);
         setTimeout(function() {
-            alert("Game Over\nYour Score: " + score)
+            gameOver = true;
         }, 650);
     }
     else {
@@ -104,4 +169,7 @@ var gameActive = setInterval(function() {
 },1000);
 
 window.addEventListener("keydown", moveSomething, false);
+window.addEventListener("keyup", stopSomething, false);
+
+
 startGame();
