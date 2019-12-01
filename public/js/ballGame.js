d9aef28e85d6ced1f782c70090d5abd81a6f8a7a
Trevor Kornaga <trevor4nowk@gmail.com>
2019-11-30 22:48:41 -0500

    Refactored code. Added classes for each object. Added logic for when timeboost and speedboost should spawn. Added MPH on bottom of screen.

--- public/js/ballGame.js
+++ public/js/ballGame.js
@@ -2,151 +2,214 @@ var canvas = document.getElementById('myCanvas');
 var label = document.getElementById('score');
 var timeLabel = document.getElementById('timeLabel');
 var progressBar = document.getElementById('progressBar');
+var spedometer = document.getElementById('spedometer');
 var ctx = canvas.getContext('2d');
-var xDim = 500;
-var yDim = 300;
 var raf;
-var speedX = 0;
-var speedY = 0;
 var score = 0;
-var timePassed = 0;
+var timePassed = 1;
 var GAME_LENGTH = 30; //seconds
-var SPEED = 1;
+var SPEED = 2;
 var MAX_SPEED = 10;
 var progressPercent = 0;
-var randomEnemyX = Math.floor(Math.random() * Math.floor(canvas.width - 30)) + 30;
-var randomEnemyY = Math.floor(Math.random() * Math.floor(canvas.height - 30)) + 30;
 
-var randomSpeedX = Math.floor(Math.random() * Math.floor(canvas.width - 30)) + 30;
-var randomSpeedY = Math.floor(Math.random() * Math.floor(canvas.height - 30)) + 30;
 label.innerText = score;
 var pointSound;
 var gameOver = false;
+var timeAlreadyCollected = false;
 
 ctx.fillStyle = '#3C3C3C';
 ctx.fillRect(0,0,canvas.width, canvas.height);
 
 var interval;
 
-function drawBall() {
-    ctx.beginPath();
-    xDim += speedX;
-    yDim += speedY;
-    if(yDim > canvas.height){
-        yDim = canvas.height;
+class Sprite {
+    constructor(canvas){
+        this.randomize();
+        this.drawn = false;
     }
-    else if (yDim < 0){
-        yDim = 0;
+    randomize() {
+        this.x = Math.floor(Math.random() * Math.floor(canvas.width - 30)) + 30;
+        this.y = Math.floor(Math.random() * Math.floor(canvas.height - 30)) + 30;
     }
-    if(xDim > canvas.width){
-        xDim = canvas.width;
+}
+class Enemy extends Sprite{
+    constructor(canvas){
+        super(canvas);
     }
-    else if (xDim < 0){
-        xDim = 0;
+    draw(ctx){
+        this.drawn = true;
+        ctx.beginPath();
+        ctx.moveTo(this.x - 20, this.y - 20);
+        ctx.lineTo(this.x + 20, this.y + 20);
+        ctx.strokeStyle = "red";
+        ctx.stroke();
+        ctx.moveTo(this.x + 20, this.y - 20);
+        ctx.lineTo(this.x - 20, this.y + 20);
+        ctx.stroke();
     }
-    ctx.arc(xDim, yDim, 25, 0, Math.PI * 2, true);
-    ctx.closePath();
-    ctx.fillStyle = "blue";
-    ctx.fill();
-};
+}
+class SpeedBoost extends Sprite{
+    constructor(canvas){
+        super(canvas)
+    }
+    draw(ctx){
+        this.drawn = true;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, 10, 0, Math.PI * 2, true);
+        ctx.closePath();
+        ctx.fillStyle = "yellow";
+        ctx.fill();
+    }
+}
+class TimeBoost extends Sprite{
+    constructor(canvas){
+        super(canvas);
+    }
+    draw(ctx){
+        this.drawn = true;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, 10, 0, Math.PI * 2, true);
+        ctx.closePath();
+        ctx.fillStyle = "green";
+        ctx.fill();
+    }
 
-function update (){
+}
+class Ball {
+    constructor(canvas, acceleration){
+        this.x = canvas.width / 2;
+        this.y = canvas.height / 2;
+        this.acceleration = acceleration
+        this.speed = {
+            x: 0,
+            y: 0
+        }
+        
+    }
+    draw() {
+        ctx.beginPath();
+        this.x += this.speed["x"];
+        this.y += this.speed["y"];
+        if(this.y > canvas.height){
+            this.y = canvas.height;
+        }
+        else if (this.y < 0){
+            this.y = 0;
+        }
+        if(this.x > canvas.width){
+            this.x = canvas.width;
+        }
+        else if (this.x < 0){
+            this.x = 0;
+        }
+        ctx.arc(this.x, this.y, 25, 0, Math.PI * 2, true);
+        ctx.closePath();
+        ctx.fillStyle = "blue";
+        ctx.fill();
+    }
+    move(e){
+        switch(e.keyCode) {
+            case 37:
+                this.speed["x"] = -this.acceleration;
+                break;
+            case 38:
+                this.speed["y"] = -this.acceleration;
+                break;
+            case 39:
+                this.speed["x"] = this.acceleration;
+                break;
+            case 40:
+                this.speed["y"] = this.acceleration;
+                break;  
+        }   
+    }
+    stop(e){
+        switch(e.keyCode) {
+            case 37:
+                this.speed["x"] = 0;
+                break;
+            case 38:
+                this.speed["y"] = 0;
+                break;
+            case 39:
+                this.speed["x"] = 0;
+                break;
+            case 40:
+                this.speed["y"] = 0;
+                break;  
+        }   
+
+    }
+}
+
+function redraw(ball, enemy, timeBoost, speedBoost, timePassed){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ball.draw(ctx);
+    enemy.draw(ctx);
 
-    drawBall();
-    drawX(randomEnemyX,randomEnemyY);
-    drawSpeed(randomSpeedX, randomSpeedY);
-    if ( (xDim - randomEnemyX < 25 && xDim - randomEnemyX > -25) &&
-         (yDim - randomEnemyY < 25 && yDim - randomEnemyY > -25) ){
-        score += 1;
-        randomEnemyX = Math.floor(Math.random() * Math.floor(canvas.width - 30)) + 30;
-        randomEnemyY = Math.floor(Math.random() * Math.floor(canvas.height - 30)) + 30;
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        drawBall();
-        drawX(randomEnemyX,randomEnemyY);
+    if(timePassed > (GAME_LENGTH/2) ){
+        if(!timeAlreadyCollected){
+            timeBoost.draw(ctx);
+        }
     }
-    if ( (xDim - randomSpeedX < 25 && xDim - randomSpeedX > -25) &&
-         (yDim - randomSpeedY < 25 && yDim - randomSpeedY > -25) ){
-        SPEED = SPEED + 1 > MAX_SPEED ? MAX_SPEED : SPEED + 1;
-        randomSpeedX = Math.floor(Math.random() * Math.floor(canvas.width - 30)) + 30;
-        randomSpeedY = Math.floor(Math.random() * Math.floor(canvas.height - 30)) + 30;
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        drawBall();
-        drawX(randomEnemyX,randomEnemyY);
-        drawSpeed(randomSpeedX, randomSpeedY);
+    if(ball.acceleration != MAX_SPEED)
+        speedBoost.draw(ctx);
+}
+function isColliding(usr, sprite){
+    if ( (usr.x - sprite.x < 25 && usr.x - sprite.x > -25) &&
+        (usr.y - sprite.y < 25 && usr.y - sprite.y > -25) ){
+            sprite.drawn = false;
+            return true;
+        }
+    return false;
+}
+
+function update (enemy, ball, speedBoost, timeBoost){
+    redraw(ball, enemy, timeBoost, speedBoost, timePassed);
+    if (isColliding(ball, enemy)){
+        score += 1;
+        enemy.randomize();
+        redraw(ball, enemy, timeBoost, speedBoost, timePassed);
+    }
+    if (isColliding(ball, speedBoost)){
+        ball.acceleration = ball.acceleration + 1 > MAX_SPEED ? MAX_SPEED : ball.acceleration + 1;
+        speedBoost.randomize();
+        redraw(ball, enemy, timeBoost, speedBoost, timePassed);
+    }
+    if (isColliding(ball, timeBoost)){
+        timePassed -= 5;
+        timeAlreadyCollected = true;
+        timeBoost.randomize();
+        redraw(ball, enemy, timeBoost, speedBoost, timePassed);
     }
     label.innerText = score;
+    spedometer.innerHTML = Math.sqrt(ball.speed.x * ball.speed.x + ball.speed.y * ball.speed.y);
     if (gameOver){
         ctx.fillStyle = "red";
         ctx.font = "30px Arial";
         ctx.fillText("GAME OVER\nScore:" + score, canvas.width/2, canvas.height/2);
         clearInterval(interval);
     }
+
 }
-
-function drawSpeed(x, y) {
-    ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI * 2, true);
-    ctx.closePath();
-    ctx.fillStyle = "yellow";
-    ctx.fill();
-};
-
-function drawX(x , y) {
-    ctx.beginPath();
-    
-    ctx.moveTo(x - 20, y - 20);
-    ctx.lineTo(x + 20, y + 20);
-    ctx.strokeStyle = "red";
-    ctx.stroke();
-
-    ctx.moveTo(x + 20, y - 20);
-    ctx.lineTo(x - 20, y + 20);
-    ctx.stroke();
-}
-
-
-
-function moveSomething(e) {
-    switch(e.keyCode) {
-        case 37:
-            speedX = -SPEED;
-            break;
-        case 38:
-            speedY = -SPEED;
-            break;
-        case 39:
-            speedX = SPEED;
-            break;
-        case 40:
-            speedY = SPEED;
-            break;  
-    }   
-}
-function stopSomething(e) {
-    switch(e.keyCode) {
-        case 37:
-            speedX = 0;
-            break;
-        case 38:
-            speedY = 0;
-            break;
-        case 39:
-            speedX = 0;
-            break;
-        case 40:
-            speedY = 0;
-            break;  
-    }   
-}
-
-
 function startGame(){
-    drawBall();
     progressBar.innerHTML = GAME_LENGTH;
     progressBar.style.width = "100%";
-    timePassed += 1;
-    interval = setInterval(update, 20);
+    //timePassed += 1;
+    var ball = new Ball(canvas, SPEED);
+    ball.draw();
+    var enemy = new Enemy(canvas);
+    var speedBoost = new SpeedBoost(canvas);
+    var timeBoost = new TimeBoost(canvas);
+    window.addEventListener("keydown", function(e) {
+        ball.move(e);
+    }, false);
+    
+    window.addEventListener("keyup", function(e){
+        ball.stop(e);
+    }, false);
+    
+    interval = setInterval(update, 20, enemy, ball, speedBoost, timeBoost);
 }
 
 var gameActive = setInterval(function() {
@@ -168,8 +231,9 @@ var gameActive = setInterval(function() {
     }
 },1000);
 
-window.addEventListener("keydown", moveSomething, false);
-window.addEventListener("keyup", stopSomething, false);
 
 
 startGame();
+
+
+
